62bc74d2eb33869b55cb57d4fb48b09abb20b69f
pakaoraki <pakaoraki@gmx.com>
2022-02-01 16:31:30 +0100

    Improve Files list: test to use a switch instead of checkbox

--- src/components/Inspector/Files/WantedButton.js
+++ src/components/Inspector/Files/WantedButton.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
 import autobind from 'autobind-decorator';
+/*import Switch from 'react-toolbox/lib/switch';*/
 
 import styles from './styles/index.css';
 
@@ -26,13 +27,17 @@ class WantedButton extends Component {
       })
     );
   }
-
+/*      <Switch
+          id={this.props.fileIds}
+          checked={this.state.checked}
+          onChange={this.onChange}
+        />*/
   render() {
     return (
       <input
-        type='checkbox'
-        onChange={this.onChange}
-        checked={this.state.checked}
+          type='checkbox'
+          onChange={this.onChange}
+          checked={this.state.checked}
       />
     );
   }
